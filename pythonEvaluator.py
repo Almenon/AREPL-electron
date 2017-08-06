734575cd6ff300f2111a78592a986b09c5682bdc
Almenon <almenon214@gmail.com>
2017-08-06 09:54:30 -0700

    added keyboardinterrupt to raised exceptions (good practice)

--- pythonEvaluator.py
+++ pythonEvaluator.py
@@ -24,7 +24,7 @@ while True:
 
     try:
         exec(data['evalCode'], evalLocals)
-    except SystemExit:
+    except (KeyboardInterrupt, SystemExit):
         raise
     except Exception:
         errorMsg = traceback.format_exc()
