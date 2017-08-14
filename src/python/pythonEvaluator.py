3ddcac2e9eee61786a6f9de8629bcf3318299096
Almenon <almenon214@gmail.com>
2017-08-14 08:34:56 -0700

    refactored python in preparation for unit testing

--- src/python/pythonEvaluator.py
+++ src/python/pythonEvaluator.py
@@ -4,18 +4,21 @@ import jsonpickle
 
 jsonpickle.set_encoder_options('json', ensure_ascii=False)
 
-
 def get_json_input():
+    """
+    input looks something like this {
+    	setting: "SETUP" | "",
+    	setupCode: "x=1", (to be used in future)
+    	evalCode: "x=1"
+    }
+    """
     return json.loads(input())
-    # input look something like this {
-    # 	setting: "SETUP" | "",
-    # 	setupCode: "x=1", (to be used in future)
-    # 	evalCode: "x=1"
-    # }
 
-while True:
-    data = get_json_input()
-
+def exec_input(codeToExec):
+    """
+    returns the jsonpickled local variables and any errors
+    {userVariables: '', ERROR: ''}
+    """
     evalLocals = {}
     returnInfo = {
         'ERROR':"",
@@ -23,7 +26,7 @@ while True:
     }
 
     try:
-        exec(data['evalCode'], evalLocals)
+        exec(codeToExec, evalLocals)
     except (KeyboardInterrupt, SystemExit):
         raise
     except Exception:
@@ -38,5 +41,12 @@ while True:
     # still has limitations but can handle much more
     returnInfo['userVariables'] = jsonpickle.encode(userVariables)
 
-    # 6q3co7 signifies to frontend that stdout is not due to a print in user's code
-    print('6q3co7' + json.dumps(returnInfo))
+    return returnInfo
+
+
+if __name__ == '__main__':
+    while True:
+        data = get_json_input()
+        returnInfoJSON = exec_input(data['evalCode'])
+        # 6q3co7 signifies to frontend that stdout is not due to a print in user's code
+        print('6q3co7' + json.dumps(returnInfoJSON))
